51f4e53a3b3dccda10f263beae8e00524562cbb6
moder <moder@DESKTOP-F8J1B3D.dhcp.wustl.edu>
2024-02-21 14:18:40 -0600

    finished studio 4

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -1,11 +1,14 @@
 package studio4;
 
-import java.awt.Color;
-
 import edu.princeton.cs.introcs.StdDraw;
 
 public class Flag {
 	public static void main(String[] args) {
-		
+	
+		double x = 0.5;
+		double y = 0.5;
+		double halfWidth = 0.5;
+		double halfLength = 0.5;
+	StdDraw.setPenColor(x, y, halfwidth, halflength);
 	}
 }
--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -19,6 +19,52 @@ public class InterpretDrawingFile {
 		chooser.showOpenDialog(null);
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
-		
+	String shapeType=in.next();
+	int redComponent=in.nextInt();
+	int blueComponent=in.nextInt();
+	int greenComponent=in.nextInt();
+	boolean isFilled=in.nextBoolean();
+	double a=in.nextDouble();
+	double b=in.nextDouble();
+	double c=in.nextDouble();
+	double d=in.nextDouble();
+	
+	StdDraw.setPenColor(redComponent, greenComponent, blueComponent);
+	if (shapeType.equals("ellipse")) {
+		if (isFilled == true);
+		StdDraw.ellipse(a, b, c, d);
 	}
+	if (shapeType.equals("ellipse")) {
+		if (isFilled == false) {
+		StdDraw.filledEllipse(a, b, c, d);}
+	}
+	if (shapeType.equals("rectangle")) {
+		if (isFilled == false) {
+		StdDraw.rectangle(a, b, c, d);}
+	}
+	if (shapeType.equals("rectangle")) {
+		if (isFilled == false) {
+		StdDraw.rectangle(a, b, c, d);}
+	}
+	if (shapeType.equals("triangle")) {
+		if(isFilled == true) {
+			double e=in.nextDouble();
+			double g=in.nextDouble();		
+			double[] x = { a, c, e};
+			double[] y = {b, d, g};
+					StdDraw.filledPolygon(x, y);
+		}
+	}
+	
+	if (shapeType.equals("triangle")) {
+		if(isFilled == false) {
+		double e=in.nextDouble();
+		double g=in.nextDouble();
+		double[] x = { a, c, e};
+		double[] y = {b, d, g};
+		StdDraw.polygon(x, y);
+		}
+	}
+	
 }
+	}
